a7a4af76df703652c7de464a651119573cd00563
Сергей Засимов <zasimovsergey@yandex.ru>
2024-04-06 00:12:11 +0300

    3.1

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,7 +2,6 @@ from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.paginator import Paginator
 from django.db.models import Count
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
